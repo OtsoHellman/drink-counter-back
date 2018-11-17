a471d2bba62988f516d7aa32bc49b2a4e99e70dd
otsohellman <otso.hellman@gmail.com>
2018-11-17 20:43:35 +0200

    Add util for calculating konni

---
+++ utils/konniCalculator.js
@@ -0,0 +1,14 @@
+const getKonni = (user, drinks) => {
+    const mass = user.mass
+    //metabolism constant
+    const MR = user.gender === 'male' ? 0.015 : 0.017
+    //body water constant
+    const BW = user.gender === 'male' ? 0.58 : 0.49
+    const numberOfDrinks = drinks.length
+    //time since first drink in hours
+    const drinkingPeriod = (Date.now() - drinks[0])/(3600*1000)
+
+    return 10 * ((0.806*numberOfDrinks*1.2)/(BW*mass) - MR*drinkingPeriod)
+}
+
+module.exports = getKonni
